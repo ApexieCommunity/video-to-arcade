21b4fcff711665bf793a0d8e71c76d856bd4eba6
ItzLightyHD <itzlightyhd@apexie.eu>
2022-04-15 17:25:31 +0200

    Getting exciting: GIFs support!

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: "none",
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname)
+    },
+    resolve: {
+        fallback: {
+            "path-browserify": require.resolve("path-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "crypto": require.resolve("crypto-browserify"),
+            "path": require.resolve("path-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
+            "buffer": require.resolve("buffer/"),
+            "assert": require.resolve("assert/"),
+            "util": require.resolve("util/")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname),
+        },
+        compress: true,
+        port: 3000,
+    }
+}
