42231627d3d64072cd0e1f18c889048717e12cd2
ItzLightyHD <itzlightyhd@apexie.eu>
2022-05-15 09:16:07 +0200

    Alright, better service worker :smile:

--- src/index.js
+++ src/index.js
@@ -13,13 +13,29 @@ const form = document.querySelector("form")
 const url = document.getElementById("url")
 const textarea = document.querySelector("textarea#code")
 
-if("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("Service worker registration successful with scope: ", registration.scope);
-    }).catch(err => {
-        console.log("Service worker registration failed: ", err);
-    });
-}
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register(
+                'sw.js',
+                {
+                    scope: '/',
+                }
+            );
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+registerServiceWorker();
 
 let originalImageSize = {
 	width: 0,
--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,18 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+const navigationRoute = new workbox.routing.NavigationRoute(
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'v2a-navigations'
+    })
 );
+  
+const imageAssetRoute = new workbox.routing.Route(({request}) => {
+    return request.destination === 'image';
+    }, new workbox.strategies.CacheFirst({
+        cacheName: 'v2a-image-assets'
+    })
+);
+  
+workbox.routing.registerRoute(navigationRoute);
+workbox.routing.registerRoute(imageAssetRoute);
